c7c789be262ddc2345c8eb6f349421470ea4c98b
pnacht <pedro.k.night@gmail.com>
2022-02-11 18:16:01 -0300

    feat: .save_connection_data saves to credential store

--- R/dot_setup_connection.R
+++ R/dot_setup_connection.R
@@ -78,6 +78,15 @@
   return(success)
 }
 
-.save_connection_data <- function() {
+.save_connection_data <- function(alias, package_id, args) {
+  alias <- paste0(package_id, alias)
 
+  # save a "csv" with all the argument names needed to define the connection
+  keyring::key_set_with_value(alias, "args",
+                              paste(names(args), collapse = ","))
+
+  # save all the necessary arguments
+  mapply(function(arg, name) {
+    keyring::key_set_with_value(alias, name, arg)
+  }, args, names(args))
 }
