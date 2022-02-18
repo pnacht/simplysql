b65a3672e2a1fda5a8b3e165746ecaff6e0993cc
pnacht <pedro.k.night@gmail.com>
2022-02-18 11:44:23 -0300

    feat: .parse_connection_args() now only parses args

--- R/dot_parse_connection_args.R
+++ R/dot_parse_connection_args.R
@@ -1,33 +1,12 @@
-.parse_connection_args <- function() {
-  drv <- readline("drv=")
+.parse_connection_args <- function(expr) {
+  if (!identical(rlang::call_fn(expr), DBI::dbConnect)) {
+    stop("The input must be a simple call to `DBI::dbConnect`.")
+  }
 
-  message("Add all other arguments in key=value form (i.e. `UID=doe`, without")
-  message("spaces around the `=`).")
-  message("Add an empty line when done.")
+  args <- rlang::call_args(expr)
 
-  args <- list(drv = drv)
-  while (TRUE) {
-    arg <- readline()
-
-    if (arg == "") {
-      break
-    }
-
-    if (!grepl("=", arg)) {
-      warning("IGNORED. All arguments must be given in key=value form.",
-              call. = FALSE,
-              immediate. = TRUE)
-      next
-    }
-
-    arg <- stringr::str_match(arg, "^(.*?)=(.*)$")
-    arg_name  <- arg[2]
-    arg_value <- arg[3]
-
-    arg <- list(arg_value)
-    names(arg) <- arg_name
-
-    args <- c(args, arg)
+  if (any(names(args) == "")) {
+    stop("All arguments given in the `DBI::dbConnect` call must be named.")
   }
 
   return(args)
