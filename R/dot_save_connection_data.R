05366273e67a7c20433ec10b0133727ae82fb7c7
pnacht <pedro.k.night@gmail.com>
2022-02-18 14:15:36 -0300

    feat: store argument types

--- R/dot_save_connection_data.R
+++ R/dot_save_connection_data.R
@@ -1,8 +1,24 @@
 .save_connection_data <- function(alias, package_id, args) {
   alias <- paste0(package_id, alias)
 
-  # save all the necessary arguments
-  mapply(function(arg, name) {
-    keyring::key_set_with_value(alias, name, arg)
-  }, args, names(args))
+  save_arg <- function(key, value) {
+    type  <- typeof(value)
+
+    if (type == "language") value <- deparse(value)
+
+    value <- glue::glue("{type}_{value}")
+    key   <- glue::glue("{key}")
+
+    keyring::key_set_with_value(
+      service  = alias,
+      username = key,
+      password = value
+    )
+  }
+
+  mapply(save_arg,
+         key   = names(args),
+         value = args)
+
+  return(invisible(NULL))
 }
