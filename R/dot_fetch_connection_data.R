50c06a8f9ca2a5e384cd9df53845d064b64fefd7
pnacht <pedro.k.night@gmail.com>
2022-02-18 14:17:23 -0300

    feat: convert connection args to given types

--- R/dot_fetch_connection_data.R
+++ R/dot_fetch_connection_data.R
@@ -7,11 +7,35 @@
     return(NULL)  # user aborted
   }
 
-  args <- sapply(keys, function(x) keyring::key_get(full_alias, x),
+  args <- sapply(keys, keyring::key_get,
+                 service = full_alias,
                  simplify = FALSE)
 
-  # convert $drv to an expression
-  args$drv <- eval(parse(text = args$drv))
+  args <- .set_connection_data_type(args)
+
+  args$drv <- eval(args$drv)
+
+  return(args)
+}
+
+.set_connection_data_type <- function(args) {
+  # set type
+  convert <- function(value, type) {
+    switch(type,
+           double   = as.double(value),
+           integer  = as.integer(value),
+           language = str2lang(value),
+           logical  = as.logical(value),
+           numeric  = as.numeric(value),
+           value)
+  }
+
+  names <- names(args)
+  regex <- stringr::str_match(args, "^([^_]*)_(.*)$")
+  args  <- mapply(convert,
+                  value = regex[, 3],
+                  type  = regex[, 2])
+  names(args) <- names
 
   return(args)
 }
