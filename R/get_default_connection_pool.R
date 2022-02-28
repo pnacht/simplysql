2cefdc62ec96dd0a0ac6dcc4f9d3abf7670ed1e7
pnacht <pedro.k.night@gmail.com>
2022-02-28 20:09:03 -0300

    feat: check for a transaction connection
    Transactions must use a single connection throughout. That connection
    will be stored as an option during the transaction, so use it if found.

--- R/get_default_connection_pool.R
+++ R/get_default_connection_pool.R
@@ -1,13 +1,19 @@
 #' @importFrom assertthat assert_that
 .get_default_connection_pool <- function() {
-  pool <- getOption("simplysql.default_connection_pool")
+  .con <- getOption("simplysql.transaction_connection")
+
+  if (!is.null(.con)) {  # in a transaction, use the same connection
+    return(.con)
+  }
+
+  .con <- getOption("simplysql.default_connection_pool")
 
   assertthat::assert_that(
-    !is.null(pool),
+    !is.null(.con),
     paste0("SQL connection has not been initialized. Call",
            "`start_sql_connection()` before attempting any SQL expressions.",
            sep = "\n  ")
   )
 
-  return(pool)
+  return(.con)
 }
